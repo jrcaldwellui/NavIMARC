a4903208b490f34dc2cf061c5166df05236f3307
Imarc <imarc@NUC.residence-rooms.uiowa.edu>
2017-06-11 19:54:55 -0500

    format_scan samples scan lidar output for much fewer range values

--- boat_sim/marc1/src/format_scan.cpp
+++ boat_sim/marc1/src/format_scan.cpp
@@ -7,7 +7,8 @@ class FormatScan
 public:
     FormatScan();
     void start();
-	void onScanMsg(const sensor_msgs::LaserScan::ConstPtr& msg);
+    void onScanMsg(const sensor_msgs::LaserScan::ConstPtr& msg);
+    int inline safeIterAdv(std::vector<float>::const_iterator& it, std::vector<float>::const_iterator& end, size_t n);
 private:
 	ros::NodeHandle n;
 	ros::Publisher newScanPub;
@@ -25,9 +26,36 @@ void FormatScan::start()
     ros::spin();
 }
 
+//https://stackoverflow.com/questions/3856416
+int inline FormatScan::safeIterAdv(std::vector<float>::const_iterator& it, std::vector<float>::const_iterator& end, size_t n)
+{
+	size_t i= 0;
+	for(; i !=n && it!=end;++i,++it);
+	return n-i;
+}
+
 void FormatScan::onScanMsg(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-	int size = msg->ranges.size();
+	//Each msg->rangs contains two rotations worth of ranges
+	//std::cout << "go time";
+	//number of scans per two rotations
+	int desiredNumOfScans = 800;
+	size_t inc = msg->ranges.size()/desiredNumOfScans;
+
+	std::vector<float> newRanges;
+	newRanges.reserve(desiredNumOfScans);
+
+	std::vector<float>::const_iterator end = msg->ranges.end();	
+	for(std::vector<float>::const_iterator it = msg->ranges.begin(); it != end;)
+	{
+		std::cout << *it;
+		newRanges.push_back(*it);
+		safeIterAdv(it,end,inc);
+	}
+	
+
+
+	int size = newRanges.size();
 	std::vector<float> intensities(size,0.0);
 	sensor_msgs::LaserScan newMsg;
 
@@ -35,12 +63,12 @@ void FormatScan::onScanMsg(const sensor_msgs::LaserScan::ConstPtr& msg)
 	newMsg.header = msg->header;
 	newMsg.angle_min = msg->angle_min;
 	newMsg.angle_max = msg->angle_max;
-	newMsg.angle_increment =msg->angle_increment;
+	newMsg.angle_increment = 6.28 / (float)newRanges.size();
 	newMsg.time_increment = msg->time_increment;
-	newMsg.scan_time = msg->scan_time;	
-	newMsg.range_min = msg->range_min;
+	newMsg.scan_time = 0.0;	
+	newMsg.range_min = 0.009999;
 	newMsg.range_max = msg->range_max;
-	newMsg.ranges = msg->ranges;
+	newMsg.ranges = newRanges;
 	// --------------
 
 	newMsg.intensities =  intensities;
